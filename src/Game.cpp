bb2927bb6298107291a70fb132e3ca0910787906
2006ngovanmanh-debug <2006ngovanmanh@gmail.com>
2026-09-07 08:46:38 +0700

    fix nut esc

--- src/Game.cpp
+++ src/Game.cpp
@@ -29,7 +29,7 @@ void Game::ChangeState(GameState newState) {
 }
 
 void Game::Run() {
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose() && !quitRequested ) {
         Update();
 
         BeginDrawing();
